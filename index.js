2d3b7c688ced7a742dc22f3f9d23a18c2288ba8f
Nithin Shanmugam V <58203293+NithinShanmugamV@users.noreply.github.com>
2025-06-08 22:40:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,88 @@
 const express = require("express");
-const { checkOrderStatus, checkOrderCustomer, checkProduct, checkCustomer, checkCustomerEmail, } = require("./handler");
+const {
+  checkOrderStatus,
+  checkOrderCustomer,
+  checkProduct,
+  checkProductAll
+} = require("./handler");
 
 const app = express();
 app.use(express.json());
 
-
 app.post("/webhook", async (req, res) => {
   const parameters = req.body.sessionInfo?.parameters || {};
   const tag = req.body.fulfillmentInfo?.tag; // This identifies the intent or step
-
   let responseMessage = "Sorry, I didn’t understand your request.";
+  let data = null;
 
   try {
     switch (tag) {
       case "check-order-status": {
-        const result = await checkOrderStatus(parameters.order_id);
-        responseMessage = result.status === 200
-          ? `Your order ${result.order_id} is ${result.delivery_status}.`
-          : result.message;
+        const result = await checkOrderStatus(parameters.orderId);
+        responseMessage =
+          result.status === 200
+            ? `Your order ${result.order_id} is ${result.delivery_status}.`
+            : result.message;
         break;
       }
 
       case "check-order-customer": {
         const result = await checkOrderCustomer(parameters.customer_id);
-        responseMessage = result.status === 200
-          ? `Your order ${result.order_id} is ${result.delivery_status}.`
-          : result.message;
+        responseMessage =
+          result.status === 200
+            ? `Your order ${result.order_id} is ${result.delivery_status}.`
+            : result.message;
         break;
       }
 
       case "check-product": {
         const result = await checkProduct(parameters.product_id);
-        console.log(result)
-        responseMessage = result.status === 200
-          ? `Product ${result.product_name} is available for ${result.cost_of_product}.`
-          : result.message;
+        responseMessage =
+          result.status === 200
+            ? `Product ${result.product_name} is available for ${result.cost_of_product}.`
+            : result.message;
         break;
       }
+      case "check-product-all": {
+        const result = await checkProductAll(parameters.product_sku);
+        if (result.status === 200){
+          data = result,
+          responseMessage =
+            "Thank you for sharing product sku. Can you also share on product detail you are looking for?";
+        }
+        else responseMessage = "We couldn't find this product for you";
 
-    //   case "check-customer": {
-    //     const result = await checkCustomer(parameters.customerId);
-    //     responseMessage = result.status === 200
-    //       ? `Account ${parameters.accountId} has balance ${result.balance}.`
-    //       : result.message;
-    //     break;
-    //   }
-    //   case "check-customer-email": {
-    //     const result = await checkCustomer(parameters.email);
-    //     responseMessage = result.status === 200
-    //       ? `Account ${parameters.accountId} has balance ${result.balance}.`
-    //       : result.message;
-    //     break;
-    //   }
+        break;
+      }
 
       default:
         responseMessage = "Unrecognized action requested.";
     }
 
-    await res.json({
-      fulfillment_response: {
-        messages: [{ text: { text: [responseMessage] } }],
-      },
-    });
+    if (tag !== "check-product-all")
+      await res.json({
+        fulfillment_response: {
+          messages: [{ text: { text: [responseMessage] } }],
+        },
+      });
+    else {
+      await res.json({
+        sessionInfo: {
+          parameters: {
+            ...data,
+          },
+        },
+        fulfillment_response: {
+          messages: [
+            {
+              text: {
+                text: [responseMessage],
+              },
+            },
+          ],
+        },
+      });
+    }
   } catch (error) {
     console.error("Webhook error:", error.message);
     await res.json({
